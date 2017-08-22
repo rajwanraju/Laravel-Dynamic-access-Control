7d08fbacd2ab76d0cf07f00a401e45376a602d0a
rajwan <rajwan.diu@gmail.com>
2017-08-23 00:54:21 +0600

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Role;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function getSignUpPage()
+    {
+        return view('auth.signup');
+    }
+
+    public function getSignInPage()
+    {
+        return view('auth.signin');
+    }
+
+    public function postSignUp(Request $request)
+    {
+        $user = new User();
+        $user->first_name = $request['first_name'];
+        $user->last_name = $request['last_name'];
+        $user->email = $request['email'];
+        $user->password = bcrypt( $request['password']);
+        $user->save();
+        $user->roles()->attach(Role::where('name', 'User')->first());
+        Auth::login($user);
+        return redirect()->route('main');
+    }
+
+    public function postSignIn(Request $request)
+    {
+        if (Auth::attempt(['email' => $request['email'], 'password' => $request['password']])) {
+            return redirect()->route('main');
+        }
+        return redirect()->back();
+    }
+
+    public function getLogout()
+    {
+        Auth::logout();
+        return redirect()->route('main');
+    }
+}
